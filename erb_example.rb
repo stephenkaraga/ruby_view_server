e0040398f13ecf2ddb2ac804efcd24415a133126
stephenkaraga <stephenkaraga@gmail.com>
2014-01-05 22:34:29 -0500

    Initial commit

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,15 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+array = ["exactly", 3, "elements!"]
+template1 = ERB.new "This array has <%= array.count%> elements."
+puts template1.result(binding)
+
+birthplace = "miami"
+statement = ERB.new "I was born in <%= birthplace.capitalize %>."
+puts statement.result(binding)
+
+tourist = {:name => "fred", :age => 17}
+tourist1 = ERB.new "In 10 years, <%= tourist[:name].capitalize %> will be <%= tourist[:age] + 10 %> years old."
+puts tourist1.result(binding)
